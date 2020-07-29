a3d9b3f1c67539e9dea63804810170fc387af17f
Saurav Chaudhary <59189555+sauravchaudharysc@users.noreply.github.com>
2020-07-29 12:54:39 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+from translate import Translator
+
+translator = Translator(to_lang="ja")
+
+try:
+	with open('./source.txt', mode='r') as my_file:
+		text = my_file.read()
+		translation = translator.translate(text)
+		print(translation)
+		with open('./destination.txt', mode='w') as my_file2:
+			my_file2.write(translation)
+
+except FileNotFoundError  as e:
+		print("Check your file path!")
